a4487e406a32c0b45bcc3e500350437a6db6f8cb
Maggie Jablinski <mjablinski@ymail.com>
2021-04-06 16:46:28 -0400

    Added parenthesis to corectly calculate the average and changed the second n2 on the third cin statement to n3.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,20 +4,24 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f; //added parenthesis to corectly calculate the average
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int num1 =0 , num2 = 0, num3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "First number: "; 
+	cin >> num1;
+	cout << "Second number: "; 
+	cin >> num2;
+	cout << "Third number: "; 
+	cin >> num3; //changed this cin from num2 to num3 
 
-	float a = average(n1, n2, n3);
+	float avg = average(num1, num2, num3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(1) << fixed << "The average is " << avg << endl;
 
+	system("pause"); 
 	return 0;
 }
